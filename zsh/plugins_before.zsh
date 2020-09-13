59ad85a1fd4fd3dcb53c1a07390760146aa3f71e
Siddharth Singh <singh.siddharth7718@gmail.com>
2020-09-13 18:30:39 +0530

    Add submodule bg-notify

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -2,3 +2,6 @@ source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # zsh-autosuggestions variables
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+
+source $HOME/.zsh/zsh-background-notify/bgnotify.plugin.zsh
+
