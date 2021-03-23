1227d76789f372548291bfd11a080f9da1482d8b
Siddharth Singh <singh.siddharth7718@gmail.com>
2021-03-23 23:29:39 +0530

    zsh: change fg for zsh-autosuggestions plugin

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -6,6 +6,7 @@ source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # zsh-autosuggestions variables
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=cyan"
 
 source $HOME/.zsh/zsh-background-notify/bgnotify.plugin.zsh
 
