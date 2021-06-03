cae6e6f5969e0cd712b9b0657c18e4ef3c2f7679
Siddharth Singh <sid@siddharthsingh77.in>
2021-06-03 18:06:55 +0530

    nvim: use rg for telescope.find_files

--- neovim/.config/nvim/after/plugin/telescope.vim
+++ neovim/.config/nvim/after/plugin/telescope.vim
@@ -2,7 +2,7 @@ if !exists(':Telescope')
   finish
 endif
 
-nnoremap ,f <cmd>lua require('telescope.builtin').find_files({previewer=false, hidden=true})<cr>
+nnoremap ,f <cmd>lua require('telescope.builtin').find_files({find_command = {"rg", "--files", "--hidden", "--glob", "!.git*"}})<cr>
 nnoremap ,g <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap g<CR> <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap ,h <cmd>lua require('telescope.builtin').help_tags()<cr>
--- neovim/.config/nvim/lua/telescopesetup.lua
+++ neovim/.config/nvim/lua/telescopesetup.lua
@@ -12,20 +12,23 @@ local find_files_custom = function(opts)
   end
 end
 
+local rg_find_cmd = {"rg", "--files", "--hidden", "--glob", "!.git*"}
+
 -- find file inside dotfiles
 custom.search_dotfiles = find_files_custom({
   cwd = "$HOME/dotfiles",
-  previewer = false,
-  hidden = true
+  find_command = rg_find_cmd
 })
 
 custom.search_scratch = find_files_custom({
   cwd = "$HOME/scratch",
+  find_command = rg_find_cmd
 })
 
 -- find file inside notes
 custom.search_notes = find_files_custom({
-  cwd = vim.g.notes_dir
+  cwd = vim.g.notes_dir,
+  find_command = rg_find_cmd
 })
 
 return custom
